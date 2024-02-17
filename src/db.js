4a08ceafe23a7f7bac3b327afaeea10cba6a090b
Gaston <ztonchyarg@hotmail.com>
2024-02-16 23:18:14 -0300

    main

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/alize`, {
+const sequelize = new Sequelize(`postgresql://postgres:Dd41fbFBBD6aE131b5fCf4FEDf46*fgc@viaduct.proxy.rlwy.net:48008/railway`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
