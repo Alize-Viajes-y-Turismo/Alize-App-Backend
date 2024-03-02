97cccb6f003e2bb5cab287ca18c75f3f9354e3be
OPNUX <gastonlegado1@gmail.com>
2024-03-02 20:21:48 -0300

    1

---
+++ src/models/Travel.js
@@ -0,0 +1,45 @@
+// Importamos la clase DataTypes de Sequelize
+const { DataTypes, Sequelize } = require('sequelize');
+
+// Exportamos una función que define el modelo de usuario
+module.exports = (sequelize) => {
+  
+    // Definimos el modelo de Person
+  const Travel = sequelize.define('Travel', {
+    // Definición de las columnas
+
+    // Columna de identificación única de la persona
+    id: {
+      allowNull: false,       // No se permite que sea nulo
+      primaryKey: true,       // Es la clave primaria
+      type: DataTypes.INTEGER, // Tipo de datos: INTEGER
+      unique: true            // Debe ser único
+    },
+    // Columna para el nombre de la persona
+    origin: {
+      type: DataTypes.STRING, // Tipo de datos: STRING
+    },
+    // Columna para el apellido de la persona
+    surname: {
+      type: DataTypes.STRING, // Tipo de datos: STRING
+    },
+    dni: {
+      type: DataTypes.STRING, // Tipo de datos: STRING
+    },
+    // Columna para el teléfono de la persona
+    phone: {
+      type: DataTypes.STRING // Tipo de datos: STRING
+    },
+    // Columna para el DNI (Documento Nacional de Identidad) de la persona
+    userId: {
+      allowNull: false,       // No se permite que sea nulo
+      foreignKey: true, 
+      type: DataTypes.INTEGER, // Tipo de datos: INTEGER
+      unique: true  
+    }
+
+  });
+
+  //Relaciones
+
+};
